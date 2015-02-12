5905897a54e214588d17df9c3ead6039d0755147
bibiana <bcristofol@gmail.com>
2015-02-12 20:34:47 +0000

    your_birthday_is_on_a_friday_in_the_year

--- lib/questions.rb
+++ lib/questions.rb
@@ -237,6 +237,8 @@ end
 # e.g. january 1st, will next be a friday in 2016
 # return the day as a capitalized string like 'Friday'
 def your_birthday_is_on_a_friday_in_the_year(birthday)
+  while (!birthday.friday?) do birthday += (60 * 60 * 24 * 365) end
+  return birthday.year
 end
 
 # in a file, total the number of times words of different lengths
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -213,10 +213,10 @@ describe 'the Friday test :)' do
   #   expect(b).to be false
   # end
 
-  # it 'your_birthday_is_on_a_friday_in_the_year' do
-  #   n = your_birthday_is_on_a_friday_in_the_year(Time.new(2013, 1, 1))
-  #   expect(n).to eq 2016
-  # end
+  it 'your_birthday_is_on_a_friday_in_the_year' do
+    n = your_birthday_is_on_a_friday_in_the_year(Time.new(2013, 1, 1))
+    expect(n).to eq 2016
+  end
 
   # it 'count_words_of_each_length_in_a_file' do
   #   n = count_words_of_each_length_in_a_file('lorem.txt') || []
